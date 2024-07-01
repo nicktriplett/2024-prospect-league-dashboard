e09160d9e2511bc9a5d6a8754da0fcdb00122212
nicktriplett <124808763+nicktriplett@users.noreply.github.com>
2024-07-01 14:43:07 -0500

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -14,7 +14,7 @@ dash.register_page(__name__, path='/')
 layout = html.Div(
     children=[
         html.H1('2024 Prospect League Dashboard',className='text-center text-dark mt-3 mb-2 fs-1'),
-        html.H1('DATA FROM GAMES THROUGH 6/24/24',className='text-center text-dark mt-2 mb-1 fs-4'),
+        html.H1('DATA FROM GAMES THROUGH 7/1/24',className='text-center text-dark mt-2 mb-1 fs-4'),
         html.P("Welcome to the 2024 Prospect League Baseball Statistics Dashboard! This dashboard has been meticulously created to present a comprehensive outlook of the 2024 Prospect League baseball season. This dashboard serves as a tool for baseball enthusiasts, analysts, and recordkeepers to explore and analyze season statistics. The goal of this dashboard is to expand upon the simplicities of the league to better understand and communicate the league and its performances among players and teams. Data has been gathered from the Prospect League website, where it was (and still is being) expanded and enhanced for further analysis and visualization through Excel and Python. Inside this dashboard, you'll discover a rich collection of statistics presented in intuitive graphs and charts. From player performance trends to team comparisons, we've curated a range of insights that will help you appreciate the talent and dynamics of the 2024 Prospect League season. To get started, use the navigation tab on the top of the page to explore different aspects of the league. I hope that you find this dashboard to be informative and satisfactory towards your needs! Enjoy your exploration of the 2024 Prospect League baseball season!",className='text-center text-dark mb-3 mt-4 fs-6'),
         dbc.Row([
             dbc.Col(
